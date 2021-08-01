9222fa6c9c151cd57f1a58bb73b323533079326c
kiyo <i.mitsuki19@gmail.com>
2021-08-01 15:48:09 +0900

    tutorial 5 about remember and button click method

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -44,13 +47,28 @@ fun Hello(name: String) {
 
 @Composable
 fun MyScreenContent(names: List<String> = listOf("tanaka", "YAMADA")) {
+    // UIを再構成する際に失いたくないデータはrememberを使用して、覚えておくことができる
+    val counterState = remember { mutableStateOf(0) }
     Column {
         for (name in names) {
             Hello("Hello,$name")
             Divider(color = Color.Black)
         }
+        Divider(color = Color.Transparent, thickness = 32.dp)
+        Counter(
+            count = counterState.value,
+            updateCount = { newCount -> counterState.value = newCount }
+        )
     }
 }
+
+@Composable
+fun Counter(count: Int, updateCount: (Int) -> Unit) {
+    Button(onClick = { updateCount(count + 1) }) {
+        Text("I've been clicked $count times")
+    }
+}
+
 
 @Preview("Text preview")
 @Composable
