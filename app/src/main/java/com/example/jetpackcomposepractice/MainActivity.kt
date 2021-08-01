8374aa01c9beb60157ccce3cf2c9030def3b7243
kiyo <i.mitsuki19@gmail.com>
2021-08-01 15:36:26 +0900

    add tutorial from 2 to 4

--- app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposepractice/MainActivity.kt
@@ -3,81 +3,59 @@ package com.example.jetpackcomposepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposepractice.ui.theme.JetpackComposepracticeTheme
 
-/**
- * 勉強用
- */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackComposepracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MyScreenContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    MaterialTheme {
-        val typography = MaterialTheme.typography
-        Column {
-            // importが3種類あるので注意すること
-            Image(
-                painter = painterResource(R.drawable.ic_launcher_background),
-                contentDescription = null,
-                modifier = Modifier
-                    .height(180.dp)
-                    .fillMaxWidth() // match-parent
-                    .clip(shape = RoundedCornerShape(20.dp)),// 画像の角を丸める
-                contentScale = ContentScale.Crop // 画像の幅が、列の幅いっぱいになるように調整する
-            )
-
-            // ImageとText Columnの間にスペースを開けるためのプロパティ
-            Spacer(Modifier.height(16.dp))
-
-            // 列を使用したい場合
-            Column {
-                Text(
-                    text = "この例では、記事のタイトルは非常に短いものでした。しかし、記事のタイトルが長すぎるような場合、それによってアプリの外観が損なわれることは望ましくありません。最初のテキスト要素を変更してみます",
-                    style = typography.h6,
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis // 長すぎる場合は、表示されるテキストを自動的に切り捨てる
-                )
-                Text(text = "I am $name", style = typography.body1)
-            }
-            // スタイルを設定したい場合
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = "Hello $name!")
-                Text(text = "I am $name")
-            }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
+fun MyApp(content: @Composable () -> Unit) {
     JetpackComposepracticeTheme {
-        Greeting("Android")
+        // Surfaceはviewの方をimportするとエラーになる
+        // 背景色の設定
+        Surface(color = Color.Yellow) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun Hello(name: String) {
+    Text(text = "Hello , $name", modifier = Modifier.padding(24.dp))
+}
+
+@Composable
+fun MyScreenContent(names: List<String> = listOf("tanaka", "YAMADA")) {
+    Column {
+        for (name in names) {
+            Hello("Hello,$name")
+            Divider(color = Color.Black)
+        }
+    }
+}
+
+@Preview("Text preview")
+@Composable
+fun preview() {
+    MyApp {
+        MyScreenContent()
     }
 }
---
+++ app/src/main/java/com/example/jetpackcomposepractice/TutorialActivity.kt
@@ -0,0 +1,87 @@
+package com.example.jetpackcomposepractice
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcomposepractice.ui.theme.JetpackComposepracticeTheme
+
+/**
+ * 勉強用
+ */
+class TutorialActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposepracticeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun Greeting(name: String) {
+    MaterialTheme {
+        val typography = MaterialTheme.typography
+        Column {
+            // importが3種類あるので注意すること
+            Image(
+                painter = painterResource(R.drawable.ic_launcher_background),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(80.dp)
+                    .fillMaxWidth() // match-parent
+                    .clip(shape = RoundedCornerShape(20.dp)),// 画像の角を丸める
+                contentScale = ContentScale.Crop // 画像の幅が、列の幅いっぱいになるように調整する
+            )
+
+            // ImageとText Columnの間にスペースを開けるためのプロパティ
+            Spacer(Modifier.height(16.dp))
+
+            // 列を使用したい場合
+            Column {
+                Text(
+                    text = "この例では、記事のタイトルは非常に短いものでした。しかし、記事のタイトルが長すぎるような場合、それによってアプリの外観が損なわれることは望ましくありません。最初のテキスト要素を変更してみます",
+                    style = typography.h6,
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis // 長すぎる場合は、表示されるテキストを自動的に切り捨てる
+                )
+                Text(text = "I am $name", style = typography.body1)
+            }
+            // BackgroundColorを設定したい場合
+            Surface(color = Color.Yellow) {
+                // スタイルを設定したい場合
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(text = "Hello $name!")
+                    Text(text = "I am $name",Modifier.padding(16.dp))
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackComposepracticeTheme {
+        Greeting("Android")
+    }
+}
